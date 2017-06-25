4bcb0db6ad4a0835cfd3afe62b5bc25ae29bb929
Nishit Majithia <nishitm@cse.iitk.ac.in>
2017-06-25 11:31:10 +0530

    Update sqli.sql

--- sqli.sql
+++ sqli.sql
@@ -10,24 +10,6 @@ GRANT ALL PRIVILEGES ON faculty. * TO 'john'@'%';
 
 grant all on faculty.* to faculty@'%' IDENTIFIED BY 'ifaculty';
 
-    CREATE TABLE IF NOT EXISTS `download` (
-            `id` int(4) NOT NULL AUTO_INCREMENT,
-            `image_name` varchar(225) NOT NULL,
-            `location` varchar(225) NOT NULL,
-            PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
-    --
-    -- Dumping data for table `download`
-    --
-
-    INSERT INTO `download` (`id`, `image_name`, `location`) VALUES
-    (1, 'Marine ford', 'images/marine.jpg'),
-    (2, 'Luffy fourth gear', 'images/Gear_Four_luffy.jpg'),
-    (3, 'Newgate Vs Teach', 'images/Newgate_Vs_Teach.jpg'),
-    (4, 'straw-hat crew', 'images/straw_hat_crew.jpg'),
-    (5, 'Whitebeard luffy ace ', 'images/Whitebeard_luffy_ace.jpg');
-
 create table `faculty`.`users` (
 `id` int(11) not NULL AUTO_INCREMENT ,
 `username` varchar(30) NOT NULL ,
